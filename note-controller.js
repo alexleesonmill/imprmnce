f18e369bc6e1bb1ca0413542ab06eb58d783f559
alexleesonmill <alexleesonmill@gmail.com>
2020-12-08 18:08:14 +0000

    note-controller can change the text from hello to a list of created notes. Next step would be to create actual functioning but can't get it to work atm.

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,27 @@
-var noteHeadline = document.getElementById('app');
-var othergreeting = 'Howdy!'
+let noteList = new NoteList
+noteList.addNote("Note 1")
+noteList.addNote("Note 2")
+noteList.addNote("Note 3")
+console.log(noteList)
+
+let listofNotes = new noteListView(noteList)
+const app = document.getElementById('app');
 
 setTimeout( function() {
-   noteHeadline.replaceWith(othergreeting);
- }, 2000);
+    app.innerHTML = listofNotes.getHTML();
+}, 2000);
+
+
+
+// class noteController {
+//     constructor(noteList) {
+//         this.noteList = noteList
+//         this.noteListView = new noteListView(this.noteList)
+//     }
+//
+//     showInHTML(element) {
+//         element.innerHTML += this.noteList.getHTML();
+//     }
+// }
+
+
